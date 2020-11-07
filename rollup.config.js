4c366e41a5320cb801b3ada651232ee50acd3729
Bernardo Gurgel <brnrdog@hey.com>
2020-11-07 12:45:42 +0100

    chore: add rollup analyzer plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-
+import analyze from "rollup-plugin-analyzer";
 
 export default {
-  input: 'src/Listbox.bs.js',
+  input: "src/Listbox.bs.js",
   output: [
     {
-      file: 'dist/listbox.cjs.js',
-      format: 'cjs',
+      file: "dist/listbox.cjs.js",
+      format: "cjs",
     },
     {
-      file: 'dist/listbox.esm.js',
-      format: 'esm',
-    }
+      file: "dist/listbox.esm.js",
+      format: "esm",
+    },
   ],
-  plugins: [commonjs(), terser()]
+  plugins: [commonjs(), terser(), analyze()],
 };
